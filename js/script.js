58045d0b55740c206cf9fbaf9fccc80efa878e87
Tyler Crosse <tylerscottcrosse@gmail.com>
2017-05-01 07:45:03 -0700

    I wish webp was universally supported 🤔

--- js/script.js
+++ js/script.js
@@ -98,20 +98,23 @@ $(document).ready(function() {
 
   $('.modalClose').click(closeModal);
 
-  $(document).keyup(function(e) {
+  $(document).keyup(function(event) {
+    event.preventDefault();
     var KEYCODE_ESC = 27;
-    if (e.keyCode === KEYCODE_ESC && modOpen) closeModal();
+    if (event.keyCode === KEYCODE_ESC && modOpen) closeModal(event);
   });
 
   // modal state
   var modOpen = false;
   var modScrollPos = 0;
 
-  function openModal() {
+  function openModal(event) {
     event.preventDefault();
     var project = projects[$(this).attr('name')];
     modScrollPos = $(window).scrollTop();
-    $('#modalImage').attr('src', project.imageUrl);
+    $('#modalWebpSrc').attr('srcset', project.imageUrl);
+    $('#modalJpgSrc').attr('srcset', project.imageFallbackUrl);
+    $('#modalImage').attr('src', project.imageFallbackUrl);
     $('#modalTech').text(project.techUsed);
     $('#modalDescription').text(project.description);
     $('#modalLive').attr('href', project.liveUrl)
@@ -120,7 +123,7 @@ $(document).ready(function() {
     modOpen = true;
   }
 
-  function closeModal() {
+  function closeModal(event) {
     event.preventDefault();
     $(window).scrollTop(modScrollPos);
     $('#modal').hide();
@@ -131,62 +134,71 @@ $(document).ready(function() {
 var projects = {
   gitterClone: {
     imageUrl: 'images/gitter-clone.gif',
+    imageFallbackUrl: 'images/gitter-clone.gif',
     techUsed: 'React, Redux, Webpack, Express, SASS, MongoDB, Jest, SemaPhoreCI, DigitalOcean',
     description: 'This is universal app was built as portfolio piece representing a subset of the features of Troupe Technology\'s wonderful chat app, Gitter. Check out the github repo to learn more!',
     liveUrl: 'https://gitter-clone.tk',
     githubUrl: 'https://github.com/tylercrosse/gitter-clone'
   },
   shortener: {
-    imageUrl: 'images/shortener.jpg',
+    imageUrl: 'images/shortener.webp',
+    imageFallbackUrl: 'images/shortener.jpg',
     techUsed: 'React, ES6, Express, SASS, MongoDB, DigitalOcean',
     description: 'bitly URL Shortener clone built in React.',
     liveUrl: 'http://104.131.109.177:3000/',
     githubUrl: 'https://github.com/tylercrosse/shortener'
   },
   garnet: {
-    imageUrl: 'images/garnet.jpg',
+    imageUrl: 'images/garnet.webp',
+    imageFallbackUrl: 'images/garnet.jpg',
     techUsed: 'Rails, RSpec, Capybara, AJAX/JSON, SASS, PostgresSQL',
     description: "Garnet is a collaboration by the instructors of GA's WDI to produce a student data-tracking app that's better than Google Sheets.",
     liveUrl: 'https://garnet.wdidc.org/sign_in',
     githubUrl: 'https://github.com/ga-dc/garnet'
   },
   spacey: {
-    imageUrl: 'images/spacey.jpg',
+    imageUrl: 'images/spacey.webp',
+    imageFallbackUrl: 'images/spacey.jpg',
     techUsed: 'D3, Rails, AJAX/JSON, RSpec, SASS, PostgresSQL',
     description: 'Space management and visualizaiton tool.',
     liveUrl: 'https://gadc.space/',
     githubUrl: 'https://github.com/ga-dc/spacey'
   },
   beacon: {
-    imageUrl: 'images/beacon.jpg',
+    imageUrl: 'images/beacon.webp',
+    imageFallbackUrl: 'images/beacon.jpg',
     techUsed: 'Express, Node, MongoDB, SASS, Facebook & Google Auth',
     description: 'Allows travlers to connect with other nearby travlers.',
     liveUrl: 'http://beacon-wdi.herokuapp.com/',
     githubUrl: 'https://github.com/tylercrosse/project-3'
   },
   atlasShared: {
-    imageUrl: 'images/atlas-shared.jpg',
+    imageUrl: 'images/atlas-shared.webp',
+    imageFallbackUrl: 'images/atlas-shared.jpg',
     techUsed: 'Angular, Express, MongoDB, SASS, Google Maps API',
     description: 'Atlas Shared makes it easier to facilitate travel recommendations from within your network by building a community of shared experiences.',
     liveUrl: 'http://atlasshared.com',
     githubUrl: 'https://github.com/tylercrosse/atlas-shared'
   },
   gitPreview: {
-    imageUrl: 'images/git-preview.jpg',
+    imageUrl: 'images/git-preview.webp',
+    imageFallbackUrl: 'images/git-preview.jpg',
     techUsed: 'Ruby, Rails, jQuery, PostgresSQL, AWS S3, Github API',
     description: 'Track visual changes between git commits',
     liveUrl: 'https://git-preview.herokuapp.com/',
     githubUrl: 'https://github.com/tylercrosse/git_preview'
   },
   flashCards: {
-    imageUrl: 'images/flash-cards.jpg',
+    imageUrl: 'images/flash-cards.webp',
+    imageFallbackUrl: 'images/flash-cards.jpg',
     techUsed: 'jQuery',
     description: 'Simple way to learn flash cards',
     liveUrl: 'http://tylercrosse.com/Flash_Card_Project/',
     githubUrl: 'https://github.com/tylercrosse/Flash_Card_Project'
   },
   scribble: {
-    imageUrl: 'images/scribble.jpg',
+    imageUrl: 'images/scribble.webp',
+    imageFallbackUrl: 'images/scribble.jpg',
     techUsed: 'Ruby, Rails, jQuery, PostgresSQL, ActiveRecord',
     description: 'Scribble is a Ruby on Rails application where users can read, write, and interact with the best content all around the world.',
     liveUrl: 'http://scribble-tyler.herokuapp.com/',
